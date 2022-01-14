a082564850c8bae9a7ed301fe06c510d77670aea
Özgür Kolukısa <ozgurkk@gmail.com>
2022-01-15 02:15:40 +0300

    RestaredVMs.ps1 filehas been changed and improved

--- RestartedVMs.ps1
+++ RestartedVMs.ps1
@@ -1,7 +1,8 @@
 ##### Environment Variables ###########
 ## Script composed by Ozgur Kolukisa
+## Purpose : This powercli script gives you a current list of vm's restarted by vSphere HA. Useful after an ESXi Host failure!
 ## mailto: ozgurkk@gmail.com
-# This script intended to use for Jenkins. To use in powershell, please comment environment variables and add variables directly.
+## Note: This script intended to use for Jenkins. To use in powershell, please comment environment variables below and add the reqiured variables directly.
 #
 $env:vCenter
 $env:Cluster
@@ -9,9 +10,10 @@ $env:Cluster
 #$Password = $env:Password | ConvertTo-SecureString -AsPlainText -Force
 #$creddentials = New-Object System.Management.Automation.PSCredential -ArgumentList #$env:UserName, $Password
 ########   Import module & Connect  ###########
-Get-Module -Name VMware* -ListAvailable | Import-Module
-Import-Module VMware.VimAutomation.Core
-Add-PSSnapin VMware.VimAutomation.Core -ErrorAction SilentlyContinue
+# You don't need to use Get-module/Import-Module and Add-pssnapin  cmdlets if you've already using powershell 7 and powercli => 10 together. If you've old versions of powershell and powercli, simply uncomment these rows.
+#Get-Module -Name VMware* -ListAvailable | Import-Module
+#Import-Module VMware.VimAutomation.Core
+#Add-PSSnapin VMware.VimAutomation.Core -ErrorAction SilentlyContinue
 Connect-VIServer $env:vCenter -User user@vpshere.local -Password *********
 #################################################################################
 Invoke-Command {
